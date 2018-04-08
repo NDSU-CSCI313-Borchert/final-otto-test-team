8857727c3cfa2320f6817f95ab4ea583af9889e9
Otto Borchert <otto.borchert@ndsu.edu>
2018-04-08 14:59:55 -0500

    first commit

---
+++ level_manager.py
@@ -0,0 +1,38 @@
+#A Singleton object
+#http://python-3-patterns-idioms-test.readthedocs.io/en/latest/Singleton.html
+
+#The LevelManager's job is to keep a stack of the currently executing screens
+class LevelManager():
+    #Inner class - This is where the implementation goes!
+    class __LevelManager:
+        def __init__(self):
+            #Prepare an empty level stack
+            self._level_stack = []
+
+        #Load a new level onto the stack
+        def load_level(self, level):
+            self._level_stack.append(level)
+
+        #Removes a level from the stack
+        def leave_level(self):
+            self._level_stack.pop()
+
+        #Peek at the top of the stack for the corrent level
+        def get_current_level(self):
+            if not self._level_stack:
+                return None
+            else:
+                return self._level_stack[-1]
+            
+    #Instance variable!
+    instance = None
+    def __init__(self):
+        #Create an object if one does not exist
+        #Note that if two constructors are called, only one object is created!
+        if not LevelManager.instance:
+            LevelManager.instance = LevelManager.__LevelManager()
+
+    #Pass attribute retrieval to the instance
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+        
---
+++ main.py
@@ -0,0 +1,46 @@
+from level_manager import *
+from title_screen import *
+
+import pygame
+import constants
+ 
+# Initialize Pygame
+pygame.init()
+screen = pygame.display.set_mode([constants.SCREEN_WIDTH,
+                                  constants.SCREEN_HEIGHT])
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+level_manager = LevelManager()
+level_manager.load_level(TitleScreen())
+ 
+# -------- Main Program Loop -----------
+while True: #Continue until quit
+
+    current_level = level_manager.get_current_level()
+
+    #We've left the TitleScreen - Exit the game
+    if current_level == None:
+        break
+
+    #Update and Draw are very common delinations in game logic
+    #Update is inolved in changing game state
+    #Draw simply draws objects on the screen
+    current_level.update()
+    current_level.draw(screen)
+ 
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # Limit to 60 frames per second
+    clock.tick(60)
+
+    #Handle base keyboard event, plus any inherited from the level    
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            break
+        current_level.handle_keyboard_event(event)
+     
+
+pygame.quit()
